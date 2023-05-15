7c2f799f53da0960c05eb3d10dafa98d2b0e853c
Vijay Krishna Palepu <vijay.0288@gmail.com>
2023-05-15 07:57:02 -0700

    Coalescing around an actual CallGraph generator in Blinky

--- src/org/spideruci/analysis/dynamic/api/CallGrapthGenerator.java
+++ src/org/spideruci/analysis/dynamic/api/CallGrapthGenerator.java
@@ -5,10 +5,13 @@ import static org.spideruci.analysis.dynamic.Profiler.REAL_OUT;
 import org.spideruci.analysis.trace.EnterExecEvent;
 import org.spideruci.analysis.trace.EventType;
 import org.spideruci.analysis.trace.InsnExecEvent;
+import org.spideruci.analysis.trace.InvokeInsnExecEvent;
 import org.spideruci.analysis.trace.MethodDecl;
 import org.spideruci.analysis.trace.TraceEvent;
 
 public class CallGrapthGenerator extends EmptyProfiler implements IProfiler {
+	
+	long count = 0;
 
 
 	@Override
@@ -23,55 +26,64 @@ public class CallGrapthGenerator extends EmptyProfiler implements IProfiler {
 
 	@Override
 	public void willInstrumentClass(final String className) {
-		REAL_OUT.print("Instrumented Class = ");
-		REAL_OUT.println(className);
-		REAL_OUT.println();
+//		REAL_OUT.print("Instrumented Class = ");
+//		REAL_OUT.println(className);
+//		REAL_OUT.println();
 	}
 
 	@Override
 	public void willInstrumentMethod(final MethodDecl e) {
-		if (e.getType() != EventType.$$method$$) {
-			return;
-		}
-		String methodName = e.getDeclName();
-		String className = e.getDeclOwner();
-		long ID = e.getId();
-		REAL_OUT.println();
-		REAL_OUT.print(e.getType() + "  ");
-		REAL_OUT.print(methodName + "  " + className);
-		REAL_OUT.println(" ID=" + ID);
+//		if (e.getType() != EventType.$$method$$) {
+//			return;
+//		}
+//		
+//		String methodName = e.getDeclName();
+//		String className = e.getDeclOwner();
+//		long ID = e.getId();
+//		REAL_OUT.println();
+//		REAL_OUT.print(e.getType() + "  ");
+//		REAL_OUT.print(methodName + "  " + className);
+//		REAL_OUT.println(" ID=" + ID);
 	}
 
 	@Override
 	public void willInstrumentCode(final TraceEvent e) {
-		if (e.getType() == EventType.$enter$ || e.getType() == EventType.$return$) {
-			REAL_OUT.println(e.getType() + " " + e.getId() + " " + e.getInsnDeclHostId());
-		} else if(e.getType() == EventType.$athrow$) {
-			REAL_OUT.println(e.getType() + " " + e.getId() + " " + e.getInsnDeclHostId());
-			return;
-		}
+//		if (e.getType() == EventType.$enter$ || e.getType() == EventType.$return$) {
+//			REAL_OUT.println(e.getType() + " " + e.getId() + " " + e.getInsnDeclHostId());
+//		} else if(e.getType() == EventType.$athrow$) {
+//			REAL_OUT.println(e.getType() + " " + e.getId() + " " + e.getInsnDeclHostId());
+//			return;
+//		}
 	}
 
 	@Override
 	public void profileMethodEntry(final EnterExecEvent e) {
-		REAL_OUT.print(e.getType());
-		REAL_OUT.print("  " + e.insnEventType);
-		REAL_OUT.print("  EventId=" + e.insnEventId);
-		REAL_OUT.print(" Calldepth=" + e.calldepth);
-		REAL_OUT.println(" Timestamp=" + e.timestamp);
+//		REAL_OUT.print(e.getType());
+//		REAL_OUT.print("  " + e.insnEventType);
+//		REAL_OUT.print("  EventId=" + e.insnEventId);
+//		REAL_OUT.print(" Calldepth=" + e.calldepth);
+//		REAL_OUT.println(" Timestamp=" + e.timestamp);
+		count += 1;
 	}
 
 	@Override
 	public void profileMethodExit(final InsnExecEvent e) {
-		REAL_OUT.print(e.getType());
-		REAL_OUT.print("  " + e.insnEventType);
-		REAL_OUT.print("  EventId=" + e.insnEventId);
-		REAL_OUT.print(" Calldepth=" + e.calldepth);
-		REAL_OUT.println(" Timestamp=" + e.timestamp);
+//		REAL_OUT.print(e.getType());
+//		REAL_OUT.print("  " + e.insnEventType);
+//		REAL_OUT.print("  EventId=" + e.insnEventId);
+//		REAL_OUT.print(" Calldepth=" + e.calldepth);
+//		REAL_OUT.println(" Timestamp=" + e.timestamp);
+	}
+	
+	@Override
+	public void profileMethodInvoke(final InvokeInsnExecEvent e) {
+		count += 1;
 	}
 
 	@Override
 	public void endProfiling() {
+		REAL_OUT.print("invoke/enter count:"); 
+		REAL_OUT.println(this.count);
 		REAL_OUT.println("endProfiling");
 	}
   
